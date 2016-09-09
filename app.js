793e4e65b73205d08693c9793c36fc7419049753
bfeng <fungbo@126.com>
2016-09-09 14:27:38 +0800

    Init project

---
+++ app.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var DsdForm = require('./dsd-form.js');
+
+ReactDOM.render(<DsdForm/>, document.getElementById('container'));
+
+
---
+++ dsd-form.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var DsdForm = React.createClass({
+    render: function () {
+        return <table>
+            <tr>
+                <th>Name</th>
+                <th>Age</th>
+            </tr>
+        </table>
+    }
+});
+
+module.exports = DsdForm;
